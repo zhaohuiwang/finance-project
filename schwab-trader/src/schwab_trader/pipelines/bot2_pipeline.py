681cde4ef1e23cac9baa6c351f675a9cfb7b153e
zhaohuiwang <ezhwang@gmail.com>
2026-07-07 22:34:20 -0500

    change order session to seamless

--- schwab-trader/src/schwab_trader/pipelines/bot2_pipeline.py
+++ schwab-trader/src/schwab_trader/pipelines/bot2_pipeline.py
@@ -202,7 +202,7 @@ class TradingBot:
 
         order = {
             "orderType": "MARKET",
-            "session": "NORMAL",
+            "session": "SEAMLESS",
             "duration": "DAY",
             "orderStrategyType": "SINGLE",
             "orderLegCollection": [
@@ -241,8 +241,8 @@ class TradingBot:
             sell_limit_price=str(cfg.limit_sell_price),
             sell_stop_price=str(stop_price),
             sell_stoplimit_price=str(round(stop_price * 0.99, 2)),
-            session_sell_limit="NORMAL",
-            session_sell_stoplimit="NORMAL",
+            session_sell_limit="SEAMLESS",
+            session_sell_stoplimit="SEAMLESS",
             duration="DAY"
         )
 
@@ -262,7 +262,7 @@ class TradingBot:
 
         order = {
             "orderType": "MARKET",
-            "session": "NORMAL",
+            "session": "SEAMLESS",
             "duration": "DAY",
             "orderStrategyType": "SINGLE",
             "orderLegCollection": [
